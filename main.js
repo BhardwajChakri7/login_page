068d15f2fb977913519dbbafef2616fd5be117b4
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2024-10-19 01:35:39 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
+import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js"
+ 
+  // Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAn2qvpJWxvhX5odYL_V4BpbTbhuLoe0Wk",
+    authDomain: "hackkare-login.firebaseapp.com",
+    projectId: "hackkare-login",
+    storageBucket: "hackkare-login.appspot.com",
+    messagingSenderId: "871908313949",
+    appId: "1:871908313949:web:3abe5a609a0f1e172d36b4"
+};
+
+  // Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+const email = document.getElementById('emailtextbox').value;
+const password = document.getElementById('passwordtextbox').value;
+const hospitalname = document.getElementById('hospitalnametextbox').value;
+const hospitaladdress = document.getElementById('hospitaladresstextbox').value;
+const signupbutton = document.getElementById('signupbtn');
+signupbutton.addEventListener("click",function(event){
+    event.preventDefault()
+    alert(5)
+})
