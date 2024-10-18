da04301f8f098e9990f8e588e0ebe60fd9441b7c
BhardwajChakri7 <155712824+BhardwajChakri7@users.noreply.github.com>
2024-10-19 03:04:06 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,38 +1,39 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.1/firebase-app.js";
-import {getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
-import{getFirestore, setDoc, doc} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-firestore.js"
- 
-  // Your web app's Firebase configuration
+import { getAuth, createUserWithEmailAndPassword} from "https://www.gstatic.com/firebasejs/10.14.1/firebase-auth.js";
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAn2qvpJWxvhX5odYL_V4BpbTbhuLoe0Wk",
-    authDomain: "hackkare-login.firebaseapp.com",
-    projectId: "hackkare-login",
-    storageBucket: "hackkare-login.appspot.com",
-    messagingSenderId: "871908313949",
-    appId: "1:871908313949:web:3abe5a609a0f1e172d36b4"
+  apiKey: "AIzaSyAn2qvpJWxvhX5odYL_V4BpbTbhuLoe0Wk",
+  authDomain: "hackkare-login.firebaseapp.com",
+  projectId: "hackkare-login",
+  storageBucket: "hackkare-login.appspot.com",
+  messagingSenderId: "871908313949",
+  appId: "1:871908313949:web:3abe5a609a0f1e172d36b4"
 };
 
-  // Initialize Firebase
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth();
 
-const email = document.getElementById('emailtextbox').value;
-const password = document.getElementById('passwordtextbox').value;
-const hospitalname = document.getElementById('hospitalnametextbox').value;
-const hospitaladdress = document.getElementById('hospitaladresstextbox').value;
 const signupbutton = document.getElementById('signupbtn');
-signupbutton.addEventListener("click",function(event){
-    const auth = getAuth();
-createUserWithEmailAndPassword(auth, email, password)
-  .then((userCredential) => {
-    // Signed up 
-    const user = userCredential.user;
-   alert("creating Account")
-    // ...
-  })
-  .catch((error) => {
-    const errorCode = error.code;
-    const errorMessage = error.message;
-   alert("errorMessage')
-    // ..
-  });
+signupbutton.addEventListener("click", function (event) {
+  event.preventDefault()
+  const email = document.getElementById('emailtextbox').value;
+  const password = document.getElementById('passwordtextbox').value;
+  const hospitalname = document.getElementById('hospitalnametextbox').value;
+  const hospitaladdress = document.getElementById('hospitaladresstextbox').value;
+  createUserWithEmailAndPassword(auth, email, password,hospitalname,hospitaladdress)
+    .then((userCredential) => {
+      // Signed up 
+      const user = userCredential.user;
+      window.location.href="https://firebase.google.com/docs/auth/web/start"
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      alert(errorMessage)
+      // ..
+    });
 })
+
